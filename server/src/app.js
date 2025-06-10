b8f54dbf0dc89a0cd972cb7bb00afc868e479ae7
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-06-10 01:25:06 +0100

    added some dependencies to setup the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+app.use(morgan("combined")); // helps print out message in a certain way
+app.use(bodyParser.json()); // allows to parsed any json received
+app.use(cors()); // helps protect from unauthorized access
+
+app.get("/status", (req, res) => {
+  res.send({ message: "Hello world" }).status(200);
+});
+app.listen(process.env.PORT || 8000);
